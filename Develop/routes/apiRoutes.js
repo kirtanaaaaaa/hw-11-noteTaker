4998543f8a398a2ce73dac5a0c05d3783f06d56b
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-07-20 15:24:07 -0400

    updates

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,15 @@
+var notes = require("../db/notes.js");
+
+module.exports = function (app) {
+    app.get("/notes", function(req, res){
+    
+    })
+
+    app.post("/notes", function(req, res){
+   
+    })
+
+    app.delete("/notes/:id", function(req, res){
+   
+    })
+}
