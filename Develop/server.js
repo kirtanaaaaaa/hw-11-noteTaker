c25c43ba9fdbb5ee04dc5ee0c39260716ec4bfcc
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-07-25 18:00:10 -0400

    updates

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var path = require("path");
-var htmlRoutes = require('./routes/htmlRoutes');
-var apiRoutes = require('./routes/apiRoutes');
+var fs = require("fs");
 
 var app = express();
 var PORT = 3000;
@@ -9,6 +8,27 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+app.get("/notes", function(req, res){
+    
+})
+
+app.post("/notes", function(req, res){
+   
+})
+
+app.delete("/notes/:id", function(req, res){
+   
+})
+
 app.listen(PORT,function(){
-    console.log("App listening on PORT " + PORT);
+        console.log("App listening on PORT " + PORT);
 })
