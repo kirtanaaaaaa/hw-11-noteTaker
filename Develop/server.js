a199c05dea9b77f2c522a0d0634972f062e5b96d
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-07-25 17:44:37 -0400

    updates

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var path = require("path");
+var htmlRoutes = require('./routes/htmlRoutes');
+var apiRoutes = require('./routes/apiRoutes');
 
 var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT,function(){
+    console.log("App listening on PORT " + PORT);
+})
